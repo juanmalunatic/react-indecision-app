46d65590e4bce24b9e985ab1409ca1496d09bf1e
juanmalunatic <juanmaluna1991@gmail.com>
2019-09-18 20:56:26 -0500

    010 Exploring JSX

--- src/app.js
+++ src/app.js
@@ -1,8 +1,25 @@
 console.log("App.js is running!");
 
 // JSX - JavaScript XML
+var template = (
+    <div>
+        <h1> Indecision App </h1>
+        <p> This is some info </p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+            <li>Item three</li>
+        </ol>
+    </div>
+);
 
-var template = <h1> Indecision App </h1>;
+var templateTwo = (
+    <div>
+        <h1> Juan Manuel </h1>
+        <p> Age: 28 </p>
+        <p> Location: Cali </p>
+    </div>
+);
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
