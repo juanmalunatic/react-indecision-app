7754a578f4c1b536e5d363fa28dbb6a5f7e5df68
juanmalunatic <juanmaluna1991@gmail.com>
2019-09-24 15:24:26 -0500

    015 ES6 Aside Arrow Functions Part II

--- src/playground/es6-arrow-function-2.js
+++ src/playground/es6-arrow-function-2.js
@@ -1,13 +1,11 @@
-'use strict';
-
 // arguments object - no longer bound
 // if you try to access arguments is not gonna work
 
 
-var add = function add(a, b) {
+const add = function (a, b) {
     console.log(arguments);
     return a + b;
-};
+}
 
 // With ES5 syntax, I can pass more than the named
 // arguments (a, b) to the function, and access the 
@@ -15,52 +13,52 @@ var add = function add(a, b) {
 
 console.log(add(55, 1, 1001));
 
-var addArrow = function addArrow(a, b) {
+const addArrow = (a, b) => {
     //console.log(arguments); // <-- this doesn't work; not bound
     return a + b;
-};
+}
 
 // --------------------------------------------
 // "this" keyword is no longer bound
 // on arrow functions
 
-var user = {
+const user = {
     name: 'Andrew',
     cities: ['Cali', 'Medellin', 'Bogota'],
-    printPlacesLived: function printPlacesLived() {
+    printPlacesLived: function () {
         // "this" is bound to this instance!
         console.log(this.name);
         console.log(this.cities);
 
-        var that = this;
+        const that = this;
         this.cities.forEach(function (city) {
             // "this" isn't bound to anything on an anon fx
-            console.log(that.name + " has lived in " + city);
+            console.log(that.name + " has lived in " + city)
             // "this" is undefined
 
             // arrow functions no longer bind their internal this
             // instead of "undefined", it uses its parent's "this" value
+        })
+
+        this.cities.forEach((city) => {
+            console.log(that.name + " has lived in " + city)
         });
 
-        this.cities.forEach(function (city) {
-            console.log(that.name + " has lived in " + city);
-        });
     },
     // changing printPlacesLived to an arrow fx
     // use the parent "this": undefined
 
-    newMethodSyntax: function newMethodSyntax(a, b) {
+    newMethodSyntax(a, b) {
         console.log(this.name + " in a different way");
     },
-    printPlacesWithMap: function printPlacesWithMap() {
-        var _this = this;
 
-        var cityMessages = this.cities.map(function (city) {
-            return _this.name + " has lived in " + city + "!";
-        });
+    printPlacesWithMap() {
+        const cityMessages = this.cities.map(
+            (city) => this.name + " has lived in " + city + "!"
+        );
         return cityMessages;
     }
-};
+}
 
 user.printPlacesLived();
 user.newMethodSyntax();
@@ -68,16 +66,14 @@ console.log(user.printPlacesWithMap());
 
 // Challenge
 
-var multiplier = {
+const multiplier = {
     numbers: [1, 2, 3],
     multiplyBy: 8,
-    multiply: function multiply() {
-        var _this2 = this;
-
-        return this.numbers.map(function (number) {
-            return number * _this2.multiplyBy;
-        });
+    multiply() {
+        return this.numbers.map(
+            (number) => number * this.multiplyBy
+        )
     }
-};
+}
 
 console.log(multiplier.multiply());
