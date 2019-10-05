45d36b343472c97adddef97c132fda84ccac45c5
Juan Manuel Luna López <juan.luna@correounivalle.edu.co>
2019-10-04 20:14:49 -0500

    018 Forms and Inputs

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,97 +5,89 @@ console.log("App.js is running!");
 var app = {
     title: "Indecision App",
     subtitle: "Put your life in the hands of a computer",
-    options: ['One', 'Two']
+    options: []
 };
 
-var template = React.createElement(
-    "div",
-    null,
-    React.createElement(
-        "h1",
-        null,
-        " ",
-        app.title,
-        " "
-    ),
-    app.subtitle && React.createElement(
-        "p",
-        null,
-        " ",
-        app.subtitle,
-        " "
-    ),
-    React.createElement(
-        "p",
-        null,
-        app.options.length > 0 ? 'Here are your options' : 'No options'
-    ),
-    React.createElement(
-        "ol",
-        null,
-        React.createElement(
-            "li",
-            null,
-            "Item one"
-        ),
-        React.createElement(
-            "li",
-            null,
-            "Item two"
-        ),
-        React.createElement(
-            "li",
-            null,
-            "Item three"
-        )
-    )
-);
+var onFormSubmit = function onFormSubmit(e) {
+    e.preventDefault();
 
-var count = 0;
-var addOne = function addOne() {
-    count++;
-    renderCounterApp();
+    var option = e.target.elements.option.value;
+
+    if (option) {
+        // An empty string is falsy
+        app.options.push(option);
+        e.target.elements.option.value = '';
+        render();
+    }
 };
-var minusOne = function minusOne() {
-    count--;
-    renderCounterApp();
-};
-var reset = function reset() {
-    count = 0;
-    renderCounterApp();
+
+var onRemoveAll = function onRemoveAll(e) {
+    app.options = [];
+    render();
 };
 
 var appRoot = document.getElementById("app");
 
-var renderCounterApp = function renderCounterApp() {
+var render = function render() {
 
-    var templateTwo = React.createElement(
+    var template = React.createElement(
         "div",
         null,
         React.createElement(
             "h1",
             null,
-            "Count: ",
-            count
+            " ",
+            app.title,
+            " "
+        ),
+        app.subtitle && React.createElement(
+            "p",
+            null,
+            " ",
+            app.subtitle,
+            " "
+        ),
+        React.createElement(
+            "p",
+            null,
+            app.options.length > 0 ? 'Here are your options' : 'No options'
+        ),
+        React.createElement(
+            "p",
+            null,
+            app.options.length
         ),
         React.createElement(
             "button",
-            { onClick: addOne },
-            "+1"
+            { onClick: onRemoveAll },
+            "Remove All"
         ),
         React.createElement(
-            "button",
-            { onClick: minusOne },
-            "-1"
+            "ol",
+            null,
+            React.createElement(
+                "li",
+                null,
+                "Item one"
+            ),
+            React.createElement(
+                "li",
+                null,
+                "Item two"
+            )
         ),
         React.createElement(
-            "button",
-            { onClick: reset },
-            "Reset"
+            "form",
+            { onSubmit: onFormSubmit },
+            React.createElement("input", { type: "text", name: "option" }),
+            React.createElement(
+                "button",
+                null,
+                "Add Option"
+            )
         )
     );
 
-    ReactDOM.render(templateTwo, appRoot);
+    ReactDOM.render(template, appRoot);
 };
-
-renderCounterApp();
+render();
