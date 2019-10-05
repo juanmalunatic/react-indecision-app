7e3552149bb1308757068315b0707bcdf00625c1
Juan Manuel Luna López <juan.luna@correounivalle.edu.co>
2019-10-04 20:32:09 -0500

    019 Arrays in JSX

--- src/app.js
+++ src/app.js
@@ -1,55 +1,56 @@
 console.log("App.js is running!");
 
 const app = {
-    title: "Indecision App",
-    subtitle: "Put your life in the hands of a computer",
-    options: []
+  title: "Indecision App",
+  subtitle: "Put your life in the hands of a computer",
+  options: []
 }
 
 const onFormSubmit = (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const option = e.target.elements.option.value;
-    
-    
-    if (option) { // An empty string is falsy
-        app.options.push(option);
-        e.target.elements.option.value = '';
-        render();
-    }
+  const option = e.target.elements.option.value;
+
+
+  if (option) { // An empty string is falsy
+    app.options.push(option);
+    e.target.elements.option.value = '';
+    render();
+  }
 }
 
 const onRemoveAll = (e) => {
-    app.options = [];
-    render();
+  app.options = [];
+  render();
 }
 
 const appRoot = document.getElementById("app");
 
 const render = () => {
 
-    const template = (
-        <div>
-            <h1> {app.title} </h1>
-            {app.subtitle && <p> {app.subtitle} </p>}
-            <p>
-                {app.options.length > 0 ? 'Here are your options' : 'No options'}
-            </p>
-            <p>
-                {app.options.length}
-            </p>
-            <button onClick={onRemoveAll}>Remove All</button>
-            <ol>
-                <li>Item one</li>
-                <li>Item two</li>
-            </ol>
-            <form onSubmit={onFormSubmit}>
-                <input type="text" name="option"/>
-                <button>Add Option</button>
-            </form>
-        </div>
-    );
+  const template = (
+    <div>
+      <h1> {app.title} </h1>
+      {app.subtitle && <p> {app.subtitle} </p>}
+      <p>
+        {app.options.length > 0 ? 'Here are your options' : 'No options'}
+      </p>
+      <p>
+        {app.options.length}
+      </p>
+      <button onClick={onRemoveAll}>Remove All</button>
+      <ol>
+        {
+          app.options.map((option) => <li key={option}>{option}</li>)
+        }
+      </ol>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option" />
+        <button>Add Option</button>
+      </form>
+    </div>
+  );
 
-    ReactDOM.render(template, appRoot);
+  ReactDOM.render(template, appRoot);
 }
 render();
